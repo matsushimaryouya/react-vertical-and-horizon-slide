59b0669f023e63a8c4b80c22acf3d7a2c947e82e
Ryouya matsushima <blue.conjrer@gmail.com>
2021-07-01 11:08:48 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// React スライダー:react-slick
+// 【公式HP】https://react-slick.neostack.com/docs/get-started/
+// 1.ライブラリインポート：npm install react-slick --save
+// ２.css：npm install slick-carousel --save
 
-function App() {
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "./App.css"
+// 3.cssのパスを通す
+import "../node_modules/slick-carousel/slick/slick.css"; 
+import "../node_modules/slick-carousel/slick/slick-theme.css";
+
+function VerticalSlide1() {
+  // 設定
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Slider {...settings}>
+        <div className="v-slide sl-1-1">
+          <h3>1-1</h3>
+        </div>
+        <div className="v-slide sl-1-2">
+          <h3>1-2</h3>
+        </div>
+        <div className="v-slide sl-1-3">
+          <h3>1-3</h3>
+        </div>
+      </Slider>
     </div>
   );
 }
+function VerticalSlide2() {
+  // 設定
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        <div className="v-slide sl-2-1">
+          <h3>2-1</h3>
+        </div>
+        <div className="v-slide sl-2-2">
+          <h3>2-2</h3>
+        </div>
+        <div className="v-slide sl-2-3">
+          <h3>2-3</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+}
+function VerticalSlide3() {
+  // 設定
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        <div className="v-slide sl-3-1">
+          <h3>3-1</h3>
+        </div>
+        <div className="v-slide sl-3-2">
+          <h3>3-2</h3>
+        </div>
+        <div className="v-slide sl-3-3">
+          <h3>3-3</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+}
+
+
+function App() {
+    // 設定
+    const settings = {
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+
+    return (
+      <div>
+        <Slider {...settings}>
+          <div className="slide">
+            <VerticalSlide1/>
+          </div>
+          <div className="slide">
+          <VerticalSlide2/>
+          </div>
+          <div className="slide">
+          <VerticalSlide3/>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 
 export default App;
